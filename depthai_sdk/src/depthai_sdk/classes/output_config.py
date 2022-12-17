803bfb81ce8c37b1f51013359c89f8971850330b
Erol444 <erol123444@gmail.com>
2022-12-17 03:28:26 +0100

    ros2 bridge wip

--- depthai_sdk/src/depthai_sdk/classes/output_config.py
+++ depthai_sdk/src/depthai_sdk/classes/output_config.py
@@ -1,16 +1,16 @@
+import subprocess
 from abc import abstractmethod
 from pathlib import Path
 from typing import Optional, Callable, List
-
 import depthai as dai
 
-from depthai_sdk import FramePacket
 from depthai_sdk.oak_outputs.syncing import SequenceNumSync
 from depthai_sdk.oak_outputs.xout import XoutFrames
 from depthai_sdk.oak_outputs.xout_base import XoutBase
 from depthai_sdk.record import Record
 from depthai_sdk.recorders.video_recorder import VideoRecorder
 from depthai_sdk.visualize import Visualizer
+import os
 
 
 class BaseConfig:
@@ -27,8 +27,7 @@ class OutputConfig(BaseConfig):
     output: Callable  # Output of the component (a callback)
     callback: Callable  # Callback that gets called after syncing
 
-    def __init__(self,
-                 output: Callable,
+    def __init__(self, output: Callable,
                  callback: Callable,
                  visualizer: Visualizer = None,
                  record_path: Optional[str] = None):
@@ -63,10 +62,7 @@ class OutputConfig(BaseConfig):
             recorder.update(Path(self.record_path), device, [xoutbase])
 
         if self.visualizer:
-            xoutbase.setup_visualize(visualizer=self.visualizer, name=xoutbase.name)
-
-        if self.record_path:
-            xoutbase.setup_recorder(recorder=recorder)
+            xoutbase.setup_visualize(self.visualizer, xoutbase.name)
 
         return [xoutbase]
 
@@ -92,20 +88,74 @@ class RecordConfig(BaseConfig):
         return [self.rec]
 
 
+class RosStreamConfig(BaseConfig):
+    outputs: List[Callable]
+    ros = None
+
+    def __init__(self, outputs: List[Callable]):
+        self.outputs = outputs
+
+    def setup(self, pipeline: dai.Pipeline, device, names: List[str]) -> List[XoutBase]:
+        xouts: List[XoutFrames] = []
+        for output in self.outputs:
+            xoutbase: XoutFrames = output(pipeline, device)
+            xoutbase.setup_base(None)
+            xouts.append(xoutbase)
+
+        # envs = os.environ
+        # if 'ROS_VERSION' not in envs:
+        #     raise Exception('ROS not found! Please install or source the ROS you would like to use.')
+        #
+        # version = envs['ROS_VERSION']
+        # if version == '1':
+        #     from depthai_sdk.integrations.ros.ros1_streaming import Ros1Streaming
+        #     self.ros = Ros1Streaming()
+        # elif version == '2':
+        from depthai_sdk.integrations.ros.ros2_streaming import Ros2Streaming
+        self.ros = Ros2Streaming()
+        # else:
+        #     raise Exception(f"ROS version '{version}' not recognized! Should be either '1' or '2'")
+        #
+        self.ros.update(device, xouts)
+        return [self]
+
+    def newMsg(self, name, msg):
+        print('RosStreamConfig new msg!', name, msg)
+        self.ros.new_msg(name, msg)
+
+    def checkQueue(self, _):
+        pass  # No queues
+
+    # def is_ros1(self) -> bool:
+    #     try:
+    #         import rospy
+    #         return True
+    #     except:
+    #         return False
+    #
+    # def is_ros2(self):
+    #     try:
+    #         import rclpy
+    #         return True
+    #     except:
+    #         return False
+
+
 class SyncConfig(BaseConfig, SequenceNumSync):
     outputs: List[Callable]
     cb: Callable
     visualizer: Visualizer
 
-    def __init__(self, outputs: List[Callable], callback: Callable):
+    def __init__(self, outputs: List[Callable], callback: Callable, visualizer: Visualizer = None):
         self.outputs = outputs
         self.cb = callback
+        self.visualizer = visualizer
 
         SequenceNumSync.__init__(self, len(outputs))
 
         self.packets = dict()
 
-    def new_packet(self, packet: FramePacket, _=None):
+    def new_packet(self, packet):
         # print('new packet', packet, packet.name, 'seq num',packet.imgFrame.getSequenceNum())
         synced = self.sync(
             packet.imgFrame.getSequenceNum(),
@@ -113,7 +163,7 @@ class SyncConfig(BaseConfig, SequenceNumSync):
             packet
         )
         if synced:
-            self.cb(synced)
+            self.cb(synced) if self.visualizer is None else self.cb(synced, self.visualizer)
 
     def setup(self, pipeline: dai.Pipeline, device: dai.Device, _) -> List[XoutBase]:
         xouts = []
@@ -122,6 +172,7 @@ class SyncConfig(BaseConfig, SequenceNumSync):
             xoutbase.setup_base(self.new_packet)
             xouts.append(xoutbase)
 
-            xoutbase.setup_visualize(Visualizer(), xoutbase.name)
+            if self.visualizer:
+                xoutbase.setup_visualize(self.visualizer, xoutbase.name)
 
         return xouts
